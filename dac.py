5cc364931aae0e136402f655def8da6349d78c90
riverfenton <river.b.fenton.th@dartmouth.edu>
2023-03-01 18:33:47 -0500

    Fixed DAC conversion pt4

--- dac.py
+++ dac.py
@@ -8,7 +8,7 @@ class dac_ops:
     def find_voltage(freq):
         max_rpm=6000/46
         out_v=freq/max_rpm*4096
-        out_v=int(round(out_v*10/6.66))
+        out_v=int(round(out_v))
         return out_v
 
     def dac_write(freq):
