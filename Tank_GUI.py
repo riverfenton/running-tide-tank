bc4e1ca4e5cd85aa0706836f587a84071b96e810
riverfenton <river.b.fenton.th@dartmouth.edu>
2023-03-01 00:23:59 -0500

    Updated amplitude window

--- Tank_GUI.py
+++ Tank_GUI.py
@@ -84,6 +84,48 @@ def read_nLf():
 #long term parameters
 [freq_list, mode_list, length_list]=read_nLf()
 
+def update_long_params():
+    update_w = Toplevel(root, bg="#6CD300")
+    
+    update_w_width = update_w.winfo_screenwidth()
+    update_w_height= update_w.winfo_screenheight()
+    update_w.geometry("%dx%d" % (update_w_width, update_w_height))
+      
+    # declaring int variable
+    # for storing name and password
+    name_var = IntVar()
+    passw_var = IntVar()
+     
+    #name=name_var.get()
+    #password=passw_var.get()
+         
+    # creating a label for
+    # name using widget Label
+    name_label = tk.Label(root, text = 'Username', font=('calibre',10, 'bold'))
+      
+    # creating a entry for input
+    # name using widget Entry
+    name_entry = tk.Entry(root,textvariable = name_var, font=('calibre',10,'normal'))
+      
+    # creating a label for password
+    passw_label = tk.Label(root, text = 'Password', font = ('calibre',10,'bold'))
+      
+    # creating a entry for password
+    passw_entry=tk.Entry(root, textvariable = passw_var, font = ('calibre',10,'normal'), show = '*')
+      
+    # creating a button using the widget
+    # Button that will call the submit function
+    sub_btn=tk.Button(root,text = 'Submit', command = submit)
+      
+    # placing the label and entry in
+    # the required position using grid
+    # method
+    name_label.grid(row=0,column=0)
+    name_entry.grid(row=0,column=1)
+    passw_label.grid(row=1,column=0)
+    passw_entry.grid(row=1,column=1)
+    sub_btn.grid(row=2,column=1)
+
 def amp_and_freq_w(freq_or_amp, list_values):
     global freq_listbox, amp_listbox, freq_list, mode_list, length_list
     
